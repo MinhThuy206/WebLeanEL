c77702bca4b35cbc3edf566cd28d0fb5d0059356
MinhThuy206 <dothibichthuy_t65@hus.edu.vn>
2023-07-11 10:36:51 +0700

    commit

---
+++ src/main/java/com/weblearnel/WebLearnElApplication.java
@@ -0,0 +1,13 @@
+package com.weblearnel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLearnElApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebLearnElApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/weblearnel/WebLearnElApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weblearnel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebLearnElApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
