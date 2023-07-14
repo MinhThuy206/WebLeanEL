463caccfaef9c19bb91f01ecadbe808b7ac3928c
MinhThuy206 <dothibichthuy_t65@hus.edu.vn>
2023-07-14 09:22:59 +0700

    commit

---
+++ src/main/java/com/weblearnel/model/User.java
@@ -0,0 +1,21 @@
+package com.weblearnel.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="User")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+    private String role;
+}
+
---
+++ src/main/java/com/weblearnel/model/Word.java
@@ -0,0 +1,5 @@
+package com.weblearnel.model;
+
+public class Word {
+
+}
