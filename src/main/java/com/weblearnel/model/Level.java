ef4a5f96edb77e941fd0d6a025f38b9e19688e3e
tringuyen <uziblue12345@gmail.com>
2023-07-16 10:30:31 +0700

    remove unused import

--- src/main/java/com/weblearnel/model/Level.java
+++ src/main/java/com/weblearnel/model/Level.java
@@ -1,14 +1,20 @@
 package com.weblearnel.model;
 
-import jakarta.persistence.*;
+import java.util.ArrayList;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 
 @Getter
 @Setter
