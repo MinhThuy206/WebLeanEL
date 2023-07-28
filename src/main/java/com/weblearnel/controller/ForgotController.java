e9181cc862593ca9a1a486dcaa017d9d46e9ce4d
SonTT119 <sonden02092002@gmail.com>
2023-07-28 10:29:19 +0700

    gửi otp

--- src/main/java/com/weblearnel/controller/ForgotController.java
+++ src/main/java/com/weblearnel/controller/ForgotController.java
@@ -1,37 +1,51 @@
 package com.weblearnel.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Random;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.weblearnel.service.ForgotService;
+// import com.weblearnel.service.ForgotService;
 
-@RestController
-// @Controller
+// @RestController
+@Controller
 public class ForgotController {
-    @Autowired
-    private ForgotService forgotservice;
+    // @Autowired
+    // // private ForgotService forgotService;
+    // private EmailService emailService;
 
-    // @GetMapping("/showForm")
-    // public String showForm() {
-    // return "forgot";
-    // }
-
-    @PostMapping("/forgot-password")
-    public String forgotPass(@RequestParam String email) {
-        String response = forgotservice.forgotPass(email);
-
-        if (!response.startsWith("Invalid")) {
-            response = "http://localhost:8080/reset-password?token=" + response;
-        }
-        return response;
+    @GetMapping("/forgot-password")
+    public String showForm() {
+        return "forgot";
     }
 
-    @PutMapping("/reset-password")
-    public String resetPass(@RequestParam String token, @RequestParam String password) {
-        return forgotservice.resetPass(token, password);
+    // @PostMapping("/forgot-password")
+    // public String forgotPass(@RequestParam String email) {
+    // String response = forgotService.forgotPass(email);
+
+    // if (!response.startsWith("Invalid")) {
+    // response = "http://localhost:8086/reset-password?token=" + response;
+    // }
+    // return response;
+    // }
+
+    // @PutMapping("/reset-password")
+    // public String resetPass(@RequestParam String token, @RequestParam String
+    // password) {
+    // return forgotService.resetPass(token, password);
+    // }
+
+    @PostMapping("/send-otp")
+    public String sendOTP(@RequestParam("email") String email) {
+        System.out.println("Email: " + email);
+        // generate otp
+        int otp = new Random().nextInt(10001, 999999);
+
+        System.out.println("OTP: " + otp);
+
+        return "verify_otp";
     }
 
 }
--- src/main/java/com/weblearnel/service/ForgotService.java
+++ src/main/java/com/weblearnel/service/ForgotService.java
@@ -1,75 +1,77 @@
-package com.weblearnel.service;
+// package com.weblearnel.service;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.util.Optional;
-import java.util.UUID;
+// import java.time.Duration;
+// import java.time.LocalDateTime;
+// import java.util.Optional;
+// import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
 
-import com.weblearnel.model.User;
-import com.weblearnel.repository.ForgotRepository;
+// import com.weblearnel.model.User;
+// import com.weblearnel.repository.ForgotRepository;
 
-@Service
-public class ForgotService {
-    private static final long EXPIRE_TOKEN = 30;
+// @Service
+// public class ForgotService {
+// private static final long EXPIRE_TOKEN = 30;
 
-    @Autowired
-    private ForgotRepository forgotRepository;
+// @Autowired
+// private ForgotRepository forgotRepository;
 
-    public String forgotPass(String email) {
-        Optional<User> userOptional = Optional.ofNullable(forgotRepository.findByEmail(email));
+// public String forgotPass(String email) {
+// Optional<User> userOptional =
+// Optional.ofNullable(forgotRepository.findByEmail(email));
 
-        if (!userOptional.isPresent()) {
-            return "Invalid email id.";
-        }
+// if (!userOptional.isPresent()) {
+// return "Invalid email id.";
+// }
 
-        User user = userOptional.get();
-        user.setToken(generateToken());
-        user.setTokenCreationDate(LocalDateTime.now());
+// User user = userOptional.get();
+// user.setToken(generateToken());
+// user.setTokenCreationDate(LocalDateTime.now());
 
-        user = forgotRepository.save(user);
-        return user.getToken();
-    }
+// user = forgotRepository.save(user);
+// return user.getToken();
+// }
 
-    public String resetPass(String token, String password) {
-        Optional<User> userOptional = Optional.ofNullable(forgotRepository.findByToken(token));
+// public String resetPass(String token, String password) {
+// Optional<User> userOptional =
+// Optional.ofNullable(forgotRepository.findByToken(token));
 
-        if (!userOptional.isPresent()) {
-            return "Invalid token";
-        }
-        LocalDateTime tokenCreationDate = userOptional.get().getTokenCreationDate();
+// if (!userOptional.isPresent()) {
+// return "Invalid token";
+// }
+// LocalDateTime tokenCreationDate = userOptional.get().getTokenCreationDate();
 
-        if (isTokenExpired(tokenCreationDate)) {
-            return "Token expired.";
+// if (isTokenExpired(tokenCreationDate)) {
+// return "Token expired.";
 
-        }
+// }
 
-        User user = userOptional.get();
+// User user = userOptional.get();
 
-        user.setPassword(password);
-        user.setToken(null);
-        user.setTokenCreationDate(null);
+// user.setPassword(password);
+// user.setToken(null);
+// user.setTokenCreationDate(null);
 
-        forgotRepository.save(user);
+// forgotRepository.save(user);
 
-        return "Your password successfully updated.";
-    }
+// return "Your password successfully updated.";
+// }
 
-    private String generateToken() {
-        StringBuilder token = new StringBuilder();
+// private String generateToken() {
+// StringBuilder token = new StringBuilder();
 
-        return token.append(UUID.randomUUID().toString())
-                .append(UUID.randomUUID().toString()).toString();
-    }
+// return token.append(UUID.randomUUID().toString())
+// .append(UUID.randomUUID().toString()).toString();
+// }
 
-    private boolean isTokenExpired(final LocalDateTime tokenCreationDate) {
+// private boolean isTokenExpired(final LocalDateTime tokenCreationDate) {
 
-        LocalDateTime now = LocalDateTime.now();
-        Duration diff = Duration.between(tokenCreationDate, now);
+// LocalDateTime now = LocalDateTime.now();
+// Duration diff = Duration.between(tokenCreationDate, now);
 
-        return diff.toMinutes() >= EXPIRE_TOKEN;
-    }
+// return diff.toMinutes() >= EXPIRE_TOKEN;
+// }
 
-}
+// }
