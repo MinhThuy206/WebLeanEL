eb5bf55a4840140140166b26e7ea3204cb451618
MinhThuy206 <dothibichthuy_t65@hus.edu.vn>
2023-07-29 11:03:59 +0700

    change submit

--- src/main/java/com/weblearnel/WebLearnElApplication.java
+++ src/main/java/com/weblearnel/WebLearnElApplication.java
@@ -8,16 +8,9 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class WebLearnElApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(WebLearnElApplication.class, args);
-    }
-
-    @Bean(name = "USER_BEAN")
-    public User setUser(){
-        User u = new User();
-        u.setUsername("admin");
-        u.setPassword("020602");
-        return u;
     }
 
 }
