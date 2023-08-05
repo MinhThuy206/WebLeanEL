499ed393131d8226adfeb05f4fa3b2674e942c2b
MinhThuy206 <dothibichthuy_t65@hus.edu.vn>
2023-08-05 13:18:05 +0700

    update profile

--- src/main/java/com/weblearnel/controller/view/UpdateProfileController.java
+++ src/main/java/com/weblearnel/controller/view/UpdateProfileController.java
@@ -4,6 +4,7 @@ import com.weblearnel.model.User;
 import com.weblearnel.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -11,9 +12,16 @@ import org.springframework.web.bind.annotation.*;
 public class UpdateProfileController {
     @Autowired
     private UserService userService;
-    @GetMapping("/update")
-    public String updateUser(@PathVariable("id") long id, @RequestBody User user) {
-        userService.updateUser(user, id);
-        return "updateProfile";
+    @GetMapping("/users/{userId}/edit")
+    public String showUpdateProfileForm(@PathVariable Long userId, Model model) {
+        User user = userService.getUserById(userId);
+        model.addAttribute("user", user);
+        return "updateProfile"; // Trả về tên của view (template) "updateProfileForm.html"
+    }
+
+    @PostMapping("/users/{userId}/edit")
+    public String updateProfile(@PathVariable Long userId, @ModelAttribute User updatedUser) {
+        userService.updateUser(updatedUser, userId);
+        return "index"; // Chuyển hướng về trang hiển thị thông tin hồ sơ sau khi cập nhật
     }
 }
