2145cb1b416fc172fb60c15b44ef21781cbea90f
MinhThuy206 <dothibichthuy_t65@hus.edu.vn>
2023-08-11 10:16:12 +0700

    sigin

--- src/main/java/com/weblearnel/controller/view/SignInController.java
+++ src/main/java/com/weblearnel/controller/view/SignInController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,34 +21,33 @@ public class SignInController {
 
     @GetMapping("/user/login")
     public String ShowsignIn() {
-        System.out.println("login");
         return "authentication/sign-in";
     }
 
     @GetMapping("/index")
     public String index() {
         System.out.println("index");
+
         return "index";
     }
 
     @PostMapping("/checklogin")
-    public ResponseEntity<String> checkLogin(@RequestBody User user){
+    public ResponseEntity<String> checkLogin(@RequestBody User user, Model model){
         try {
             String username = user.getUsername();
             String password = user.getPassword();
             User userCheck = userService.getUser(username);
             if(userCheck.getPassword().equals(password)){
                 System.out.println("login thanh cong");
+                model.addAttribute("user", userCheck);
                 return ResponseEntity.ok("redirect:/index");
             }
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error submitting the form: " + "Sai mật khẩu");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Login failed");
         } catch (Exception e) {
-            System.out.println("login that bai");
+            System.out.println("login failed ");
             System.out.println(e.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error submitting the form: " + e.getMessage());
-        } 
-        
-        
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Login failed");
+        }
     }
 
     @GetMapping("/logout")
