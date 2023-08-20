4ce86d4993cdfcbae49ea83d8d717f9951e63c7f
tringuyen <uziblue12345@gmail.com>
2023-08-20 12:13:24 +0700

    fix test-eng3

--- src/main/java/com/weblearnel/controller/view/ExamTestController.java
+++ src/main/java/com/weblearnel/controller/view/ExamTestController.java
@@ -1,5 +1,4 @@
 package com.weblearnel.controller.view;
-
 import java.util.List;
 import java.util.Map;
 
@@ -18,6 +17,7 @@ import com.weblearnel.model.Exam;
 import com.weblearnel.model.Question;
 import com.weblearnel.model.Result;
 import com.weblearnel.model.User;
+import com.weblearnel.repository.ResultRepository;
 import com.weblearnel.service.AnswerService;
 import com.weblearnel.service.ExamService;
 import com.weblearnel.service.QuestionService;
@@ -44,8 +44,12 @@ public class ExamTestController {
     @Autowired
     private ExamService examService;
 
+    @Autowired
+    private ResultRepository resultRepository;
+
     @GetMapping("/exam/overview/{user_id}/{exam_id}")
-    public String showExamOverView(@PathVariable("user_id") Long user_id, Model model,@PathVariable("exam_id") Long exam_id) {
+    public String showExamOverView(@PathVariable("user_id") Long user_id, Model model,
+            @PathVariable("exam_id") Long exam_id) {
         User user = userService.getUserById(user_id);
         model.addAttribute("user", user);
         Exam exam = examService.getExamById(exam_id);
@@ -54,7 +58,8 @@ public class ExamTestController {
     }
 
     @GetMapping("/exam/test/{user_id}/{exam_id}")
-    public String showExamTest(@PathVariable("user_id") Long user_id, Model model, @PathVariable("exam_id") Long exam_id) {
+    public String showExamTest(@PathVariable("user_id") Long user_id, Model model,
+            @PathVariable("exam_id") Long exam_id) {
         Exam exam = examService.getExamById(exam_id);
         model.addAttribute("exam", exam);
         User user = userService.getUserById(user_id);
@@ -107,7 +112,7 @@ public class ExamTestController {
     }
 
     @GetMapping("/exam/test_eng1/{user_id}/{exam_id}")
-    public String Eng1Test(@PathVariable("user_id") Long user_id, Model model, @PathVariable("exam_id" ) Long exam_id) {
+    public String Eng1Test(@PathVariable("user_id") Long user_id, Model model, @PathVariable("exam_id") Long exam_id) {
         User user = userService.getUserById(user_id);
         model.addAttribute("user", user);
         Exam exam = examService.getExamById(exam_id);
@@ -116,14 +121,12 @@ public class ExamTestController {
     }
 
     @GetMapping("/exam/test_eng2/{user_id}/{exam_id}")
-    public String Eng2Test(@PathVariable("user_id") Long user_id,@PathVariable("exam_id" ) Long exam_id
-                           ,Model model) {
+    public String Eng2Test(@PathVariable("user_id") Long user_id, @PathVariable("exam_id") Long exam_id, Model model) {
         List<Question> question1 = examService.getQuestions(exam_id);
         model.addAttribute("questionList", question1);
         User user = userService.getUserById(user_id);
         model.addAttribute("user", user);
-        for (Question q: question1
-             ) {
+        for (Question q : question1) {
             System.out.println(q.getAnswer());
         }
         Exam exam = examService.getExamById(exam_id);
@@ -131,34 +134,47 @@ public class ExamTestController {
         return "exam/test-eng2";
     }
 
-   @GetMapping("/exam/test_eng3/{user_id}/{exam_id}")
-   public String Eng3Test(@PathVariable("user_id") Long user_id, Model model, @PathVariable("exam_id") Long exam_id) {
-       User user = userService.getUserById(user_id);
-       model.addAttribute("user", user);
-       Exam exam = examService.getExamById(exam_id);
-       model.addAttribute("exam", exam);
-       return "exam/test-eng3";
-   }
+    @GetMapping("/exam/test_eng3/{user_id}/{exam_id}")
+    public String Eng3Test(@PathVariable("user_id") Long user_id, Model model, @PathVariable("exam_id") Long exam_id) {
+        User user = userService.getUserById(user_id);
+        model.addAttribute("user", user);
+        Exam exam = examService.getExamById(exam_id);
+        model.addAttribute("exam", exam);
+        // Set<Result> results = exam.getResults();
+        // List<LocalDateTime> dates = new ArrayList<>();
+        // for (Result result : results) {
+        //     dates.add(result.getResultDatetime());
+        // }
+        // List<Result> results2 = new ArrayList<>(results);
+        // results2.sort((Result r1, Result r2) -> r1.getResultDatetime().compareTo(r2.getResultDatetime()));
+        Result latest = resultRepository.findLatestResultOfSameExamAndUser(exam_id, user_id);
+        model.addAttribute("latest", latest);
+        // LocalDateTime latesTime = Collection.max(dates);
+        // Comparator<Object> datetimeComparator = Comparator
+        //         .comparing(result -> result.getDatetime().minusSeconds(currentTime.toEpochSecond()));
+        return "exam/test-eng3";
+    }
 
     @PostMapping("/exam/submit/{email}/{examId}")
-    public ResponseEntity<String> submitExam(@RequestBody List<Map<String, Object>> userAnswers, @PathVariable("email") String email,
+    public ResponseEntity<String> submitExam(@RequestBody List<Map<String, Object>> userAnswers,
+            @PathVariable("email") String email,
             Model model, @PathVariable("examId") Long examId) {
         User user = userService.getUserByEmail(email);
         System.out.println(userAnswers);
         try {
-            for (Map<String, Object> userAnswer: userAnswers) {
-            Answer answer = new Answer();
-            examId = Long.valueOf(userAnswer.get("examId").toString());
+            for (Map<String, Object> userAnswer : userAnswers) {
+                Answer answer = new Answer();
+                examId = Long.valueOf(userAnswer.get("examId").toString());
 
-            if (userAnswer.containsKey("questionId")) {
-                Long questionId = Long.valueOf(userAnswer.get("questionId").toString());
-                Question question = questionService.getQuestionById(questionId);
-                answer.assignQuestion(question);
-                answer.setUserAnswer(userAnswer.get("userAnswer").toString());
-                answer.assignUser(user);
-                answerService.addAnswer(answer);
+                if (userAnswer.containsKey("questionId")) {
+                    Long questionId = Long.valueOf(userAnswer.get("questionId").toString());
+                    Question question = questionService.getQuestionById(questionId);
+                    answer.assignQuestion(question);
+                    answer.setUserAnswer(userAnswer.get("userAnswer").toString());
+                    answer.assignUser(user);
+                    answerService.addAnswer(answer);
+                }
             }
-        }
             Exam exam = examService.getExamById(examId);
             List<Answer> userAnswersCheck = answerService.getAnswersByUserId(user.getId());
             double score = answerService.checkAnswers(userAnswersCheck, user.getId());
@@ -173,10 +189,9 @@ public class ExamTestController {
             // return "redirect:/exam/test-eng3" + "/" + user.getId() + "/" + examId;
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("Error submitting the form: " );
+                    .body("Error submitting the form: ");
         }
-        
-    }
 
+    }
 
 }
--- src/main/java/com/weblearnel/repository/ResultRepository.java
+++ src/main/java/com/weblearnel/repository/ResultRepository.java
@@ -11,5 +11,8 @@ import com.weblearnel.model.Result;
 public interface ResultRepository extends JpaRepository<Result, Long>{
     @Query(value = "SELECT * FROM Result u WHERE u.fk_ex_id = ?1", nativeQuery = true)
     Set<Result> findResultsOfSameExam(@PathVariable Long ex_id);
+
+    @Query(value = "SELECT * FROM result r where r.datetime = (SELECT MAX(datetime) FROM Result u WHERE u.fk_ex_id = ?1 AND u.fk_user_id = ?2)", nativeQuery = true)
+    Result findLatestResultOfSameExamAndUser(@PathVariable Long ex_id, @PathVariable Long user_id);
     
 }
