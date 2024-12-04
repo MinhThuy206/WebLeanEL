80b9565f3009c643a44cb53e0c38cd031e774c36
tringuyen <tringuyen874282@gmail.com>
2024-12-04 17:03:06 +0700

    fix bug not get css

--- src/main/java/com/weblearnel/security/SecurityConfig.java
+++ src/main/java/com/weblearnel/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.weblearnel.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -29,13 +30,18 @@ public class SecurityConfig {
     }
 
     @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder);
+        return provider;
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
                         .requestMatchers(request -> request.getServletPath().equals("/login")).permitAll()
-                        .requestMatchers(request -> request.getServletPath().startsWith("/assets/css/")).permitAll()
-                        .requestMatchers(request -> request.getServletPath().startsWith("/assets/js/")).permitAll()
-                        .requestMatchers(request -> request.getServletPath().startsWith("/assets/images/")).permitAll()
-
+                        .requestMatchers(request -> request.getServletPath().startsWith("/assets/")).permitAll()
                         .anyRequest().authenticated())
                 .formLogin(form -> form.loginPage("/login").permitAll()
                         .defaultSuccessUrl("/Word-Learning", true)
